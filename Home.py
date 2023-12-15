b1caa1b8c01047bbd76fe1619045bd98f786dfbe
Hayari4455 <151019509+Hayari4455@users.noreply.github.com>
2023-12-15 14:41:45 +0700

    Add files via upload

--- Home.py
+++ Home.py
@@ -4,3 +4,12 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
 st.subheader('สาขาวิชาวิทยาการข้อมูล')
 st.markdown("----")
+
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/oir.jpg')
+with col2:
+    st.image('./pic/ddd.png')
