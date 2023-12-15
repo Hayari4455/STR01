af632d4f6fb58c2bae3cbd5eeb7c03f990848a61
Hayari4455 <hayarinoben@gmail.com>
2023-12-15 14:19:28 +0700

    Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.header("Atithep Komutchay")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
+st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.markdown("----")
