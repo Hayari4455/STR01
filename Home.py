ee0dfa6380319e9bfbe6775aa07439f8286300e2
Hayari4455 <hayarinoben@gmail.com>
2023-12-15 15:28:02 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,35 +25,6 @@ st.markdown("")
 import pandas as pd
 
 dt=pd.read_csv('./data/iris.csv')
-
-import streamlit as st
-
-st.title('การทดสอบเขียนเว็บด้วย Python')
-st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
-st.subheader('สาขาวิชาวิทยาการข้อมูล')
-st.markdown("----")
-
-
-col1, col2 = st.columns(2)
-#col1.write("This is column 1")
-#col2.write("This is column 2")
-with col1:
-    st.image('./pic/oir.jpg')
-with col2:
-    st.image('./pic/ddd.png')
-
-    html_1 = """
-<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
-</div>
-"""
-st.markdown(html_1, unsafe_allow_html=True)
-st.markdown("")
-
-import pandas as pd
-
-dt=pd.read_csv('./data/iris.csv')
-
 st.write(dt.head(10))
 
 dt1 = dt['petal.length'].sum()
