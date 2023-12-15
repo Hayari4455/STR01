9dcb5dc81f3bd1d48ff3df07ec2abfc8975baea9
Hayari4455 <hayarinoben@gmail.com>
2023-12-15 15:44:43 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -65,7 +65,7 @@ import numpy as np
 
 if st.button("ทำนายผล"):
    # ทำนาย
-   #dt = pd.read_csv("./data/iris.csv") 
+   dt = pd.read_csv("./data/iris.csv") 
 
    X = dt.drop('variety', axis=1)
    y = dt.variety   
